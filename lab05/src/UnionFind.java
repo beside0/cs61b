e0be91175d6734cf06212aacfca5aa196a9738d2
wangyv <wangyv123@sjtu.edu.cn>
2024-09-05 15:07:10 +0800

    lab05 finished

--- lab05/src/UnionFind.java
+++ lab05/src/UnionFind.java
@@ -3,35 +3,46 @@ public class UnionFind {
 
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
+    private  int[] disjoint;
     public UnionFind(int N) {
-        // TODO: YOUR CODE HERE
+        disjoint=new int[N];
+        for(int i=0;i<N;i++){
+            disjoint[i]=-1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        v=find(v);
+        return -disjoint[v];
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return disjoint[v];
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO: YOUR CODE HERE
-        return false;
+        int v11=find(v1);
+        int v12=find(v2);
+        return v11==v12;
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. If invalid items are passed into this
        function, throw an IllegalArgumentException. */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        if (v < 0 || v >= disjoint.length) {
+            throw new IllegalArgumentException("IllegalArgument!");
+        }
+        if (parent(v) < 0) {
+            return v;
+        } else {
+            disjoint[v] = find(parent(v));
+            return parent(v);
+        }
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -40,7 +51,20 @@ public class UnionFind {
        root to V2's root. Union-ing an item with itself or items that are
        already connected should not change the structure. */
     public void union(int v1, int v2) {
-        // TODO: YOUR CODE HERE
+        int v11=find(v1);
+        int v12=find(v2);
+        if(v11==v12){
+            return;
+        }
+        if(sizeOf(v11)>sizeOf(v2)){
+            disjoint[v11]+=disjoint[v12];
+            disjoint[v12]=v11;
+        }
+        else if(sizeOf(v11)<=sizeOf(v2)) {
+            disjoint[v12] += disjoint[v11];
+            disjoint[v11] = v12;
+        }
     }
+
 
 }
